aa6011e4f661501d504af1230a052baf3c97ae29
rromano <rromanomusic@gmail.com>
2019-03-13 11:13:59 +1100

    Soldier Logic: Bleeding complete

--- MashGameUnity/Assets/Scripts/Soldier.cs
+++ MashGameUnity/Assets/Scripts/Soldier.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class Soldier : MonoBehaviour {
 
-//	private void OnTriggerEnter2D(Collider2D col){
-//		if(col.gameObject.tag.Equals("Helicopter")){
-//			Destroy(this.gameObject);
-//		}
-//	}	
+	[SerializeField]private float deathTime;
+	private float bleedTime;
+
+	void Start(){
+		bleedTime = deathTime;
+	}
+
+	void Update(){
+		Bleeding();
+	}
+
+	private void Bleeding(){
+		bleedTime-= Time.deltaTime;
+		if(bleedTime<=0){
+			Die();
+		}
+	}
+
+	private void Die(){
+		Destroy(this.gameObject);
+	}
 }
